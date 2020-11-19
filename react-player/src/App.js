7701230d0dd95f7ad347d216fad81cea7a384ca8
yfove <curtis.ch.chat@gmail.com>
2020-11-18 21:27:21 -0700

    initial style

--- react-player/src/App.js
+++ react-player/src/App.js
@@ -1,11 +1,18 @@
+import React from "react";
+// Import Styles
+import "./styles/app.scss";
+//Adding Components
+import Player from './components/Player';
+import Song from './components/Song';
 
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <Song />
+      <Player />
     </div>
-  );                            
+  );
 }
 
 export default App;
