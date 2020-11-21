5cae02078958e84e1f53f4c2801680c41e484cf6
yfove <curtis.ch.chat@gmail.com>
2020-11-20 21:05:02 -0700

    create component, add app data, add our state

--- react-player/src/App.js
+++ react-player/src/App.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 // Import Styles
 import "./styles/app.scss";
 //Adding Components
 import Player from './components/Player';
 import Song from './components/Song';
+//Import Util
+import data from "./data";
 
 
 function App() {
+  //State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong}/>
       <Player />
     </div>
   );
-}
+};
 
 export default App;
 
